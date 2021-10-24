f1fbd0b459fb0d6fa654ba405276fb6f8ca06620
C10udburst <18114966+C10udburst@users.noreply.github.com>
2021-10-24 21:47:48 +0200

    initial commit

---
+++ SendEmbeds/index.js
@@ -0,0 +1,53 @@
+import { commands } from "@goosemod/patcher";
+import { findByProps } from "@goosemod/webpackModules";
+
+let MessageParser = findByProps("createBotMessage")
+let MessageQueue = findByProps("enqueue")
+let StoreChannels = findByProps("getChannelId")
+
+export default {
+    goosemodHandlers: {
+        onImport: () => {
+            commands.add("jsonembed","Send Embeds", (args) => {
+                var discordEmbed = null
+                try {
+                    discordEmbed = JSON.parse(args["json"][0]["text"])
+                    if (discordEmbed.color !== undefined
+                          && discordEmbed.color.match !== undefined
+                          && discordEmbed.color.match(/^#?[0-9A-Z]{6}/gi) !== undefined) {
+                          discordEmbed.color = discordEmbed.color.replace(/#/g, "");
+                          discordEmbed.color = parseInt(discordEmbed.color, 16);
+                      }
+                } catch (e) {
+                    goosemod.showToast(`Failed to parse your input: ${e}`)
+                    return
+                }
+                try {
+                      let channelID = StoreChannels.getChannelId();
+                      let msg = MessageParser.createBotMessage(channelID, '');
+            
+                      MessageQueue.enqueue({
+                        type: 0,
+                        message: {
+                          channelId: channelID,
+                          content: '',
+                          tts: false,
+                          nonce: msg.id,
+                          embed: discordEmbed
+                        }
+                      }, r => {
+                        if (r.ok !== true) {
+                         goosemod.showToast(`Failed to send the message: ${r.body.message}`)
+                        }
+                      })
+                    } catch (e) {
+                      goosemod.showToast(`Failed to send the message: ${e}`)
+                    }
+            },
+            [{ name: "json", type: 3, required: true }])
+        },
+        onRemove: () => {
+            commands.remove("jsonembed");
+        }
+    }
+}
